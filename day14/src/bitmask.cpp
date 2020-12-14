6b10b21becb026c59c95ca0a37c63b260631418f
raquelfishes <raquelpm23@gmail.com>
2020-12-14 19:51:34 +0100

    Solution day 14, part 2

--- day14/src/bitmask.cpp
+++ day14/src/bitmask.cpp
@@ -30,6 +30,45 @@ uint64_t applyMask( const std::string& mask, const std::string value )
   return originalValue;
 }
 
+void getMaskPositions( const std::string& mask, const std::string value, std::vector<uint64_t>& newValues )
+{
+  uint64_t originalValue = std::stoull( value );
+  std::vector<uint64_t> xMask;
+  for ( auto i = 0; i < mask.size(); ++i )
+  {
+    uint64_t auxIndex = mask.size() - 1 - i;
+    const uint64_t auxMask{ static_cast<uint64_t>( 1 ) << static_cast<uint64_t>( i ) };
+    if ( mask[auxIndex] == '1' )
+    {
+      originalValue |= auxMask;
+    }
+    else if ( mask[auxIndex] == 'X' )
+    {
+      xMask.push_back( auxMask );
+      originalValue &= ~auxMask;
+    }
+  }
+
+  newValues.clear();
+  newValues.push_back( originalValue );
+  // We have at originalValue the default mask without apply X
+  // Num possible combi is 2**xPos.size();
+  std::vector<uint64_t> auxValues;
+  for ( auto i = 0; i < xMask.size(); ++i )
+  {
+    auxValues.clear();
+    for ( auto& newValue : newValues )
+    {
+      auxValues.push_back( newValue | xMask[i] );
+      auxValues.push_back( newValue & ~xMask[i] );
+    }
+    newValues.clear();
+    newValues.resize( auxValues.size() );
+    std::copy( auxValues.begin(), auxValues.end(), newValues.begin() );
+  }
+
+}
+
 
 void adventDay14()
 {
@@ -80,7 +119,32 @@ void adventDay14()
   }
   std::cout << "Part1: The result is: " << result << std::endl;
 
-  
+
+
+  memory.clear();
+  int numX = 0;
+  for ( auto& instr : instructions )
+  {
+    if ( std::regex_search( instr, sm, maskExpr ) )
+    {
+      currentMask = sm[1];
+      numX = std::count( currentMask.begin(), currentMask.end(), 'X' );
+    }
+    else if ( std::regex_search( instr, sm, memExpr ) )
+    {
+      std::vector<uint64_t> positions;
+      getMaskPositions( currentMask, sm[1].str(), positions );
+      for ( auto& pos : positions )
+      {
+        memory[pos] = std::stoull(sm[2].str());
+      }
+    }
+  }
+  result = 0;
+  for ( auto& mem : memory )
+  {
+    result += mem.second;
+  }
   std::cout << "Par2: The result is: " << result << std::endl;
 
 }
