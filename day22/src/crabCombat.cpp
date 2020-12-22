47d828dbb9011421d088d9367d7460386f9cf30f
Raquel <raquelpm23@gmail.com>
2020-12-22 18:08:31 +0100

    Solution day 22 part 2

--- day22/src/crabCombat.cpp
+++ day22/src/crabCombat.cpp
@@ -17,6 +17,7 @@ class Player
 public:
   Player() = default;
   Player( const int& id ) : id_( id ) {};
+  Player( const int& id, std::vector<int> cards ) : id_( id ), cards_(cards) {};
 
   void addCards( const std::vector<int>& cards )
   {
@@ -37,7 +38,7 @@ public:
   {
     int result = cards_.front();
     cards_.erase( cards_.begin() );
-    std::cout << "Player " << id_ << " plays: " << result << std::endl;
+    //std::cout << "Player " << id_ << " plays: " << result << std::endl;
     return result;
   }
 
@@ -57,13 +58,87 @@ public:
     for ( const auto& i : cards_ )
       std::cout << i << ' ';
     std::cout << std::endl;
+  }
 
+  bool checkHistory()
+  {
+    const int config = std::count ( history_.begin(), history_.end(), cards_);
+    return config == 0;
+  }
+
+  void saveHistory()
+  {
+    history_.push_back( cards_ );
   }
 
   int id_;
   std::vector<int> cards_;
 
+  std::vector<std::vector<int>> history_;
+
 };
+
+
+int game( const int g, Player& p1, Player& p2, const bool recursive=false )
+{
+  //std::cout << "== Game " << g << " ==" << std::endl;
+  bool playing = true;
+  int round = 1;
+  while ( playing )
+  {
+    //std::cout << "-- Round " << round << " Game (" << g << ") --" << std::endl;
+
+    if (recursive && ( !p1.checkHistory() || !p2.checkHistory() ))
+    {
+      //std::cout << " Player 1 wins round " << round << " of game " << g << "! " << std::endl;
+      return p1.id_;
+    }
+    p1.saveHistory();
+    p2.saveHistory();
+
+    //p1.printDeck();
+    //p2.printDeck();
+
+    int play1 = p1.playCard();
+    int play2 = p2.playCard();
+
+    int winId = -1;
+    // Is recursive ??
+    if ( recursive && ( p1.cards_.size() >= play1 ) && ( p2.cards_.size() >= play2 ) )
+    {
+      //std::cout << "Playing a sub-game to determine the winner..." << std::endl;
+      std::vector<int> cards1(p1.cards_.begin(), p1.cards_.begin()+play1);
+      std::vector<int> cards2( p2.cards_.begin(), p2.cards_.begin() + play2 );
+      winId = game( g+1, Player( p1.id_, cards1 ), Player( p2.id_, cards2 ), recursive );
+      //std::cout << "...anyway, back to game " << g << "." << std::endl;
+    }
+    else if ( play1 > play2 )
+    {
+      winId = p1.id_;      
+    }
+    else if ( play2 > play1 )
+    {
+      winId = p2.id_;      
+    }
+
+    if ( winId == p1.id_ )
+    {
+      p1.addCards( std::vector<int>{play1, play2} );
+    }
+    else if ( winId == p2.id_ )
+    {
+      p2.addCards( std::vector<int>{play2, play1} );
+    }
+
+    //std::cout << " Player " << winId << " wins round " << round << " of game " << g << "! " << std::endl;
+    playing = p1.hasCards() && p2.hasCards();
+    round++;
+    //std::cout << std::endl;
+  }
+
+  return p1.getResult() ? p1.id_ : p2.id_;
+
+}
 
 
 void adventDay22()
@@ -80,7 +155,6 @@ void adventDay22()
   Player player2(2);
 
 
-  std::map<int, Player> players;
   std::string line;
   std::regex expr( "Player (\\d+):" );
   std::smatch sm;
@@ -106,44 +180,20 @@ void adventDay22()
     }
   }
   
+  //game( 1, player1, player2, false );
+ 
+  //std::cout << "== Post-game results ==" << std::endl;
 
-  for ( auto& player : players )
-  {
-    player.second.id_ = player.first;
-  }
-
-  bool playing = true;
-  int round = 1;
-  while ( playing )
-  {
-    std::cout << "-- Round " << round << " --" << std::endl;
-
-    player1.printDeck();
-    player2.printDeck();
-
-    int play1 = player1.playCard();
-    int play2 = player2.playCard();
-
-    if ( play1 > play2 )
-    {
-      player1.addCards( std::vector<int>{play1, play2} );
-      std::cout << " Player 1 wins the round! " << std::endl;
-    }
-    else
-    {
-      player2.addCards( std::vector<int>{play2, play1} );
-      std::cout << " Player 2 wins the round! " << std::endl;
-    }
-    playing = player1.hasCards() && player2.hasCards();
-    std::cout << " " << std::endl;
-  }
+  //long long result = player1.getResult() + player2.getResult();
+  //std::cout << "Part 1:  " << result << std::endl;
+  std::cout << std::endl;
+  std::cout << std::endl;
+  
+  game( 1, player1, player2, true );
 
   std::cout << "== Post-game results ==" << std::endl;
 
   long long result = player1.getResult() + player2.getResult();
-  std::cout << "Part 1:  " << result << std::endl;
-  
-
 
   std::cout << "Part 2:  " << result << std::endl;
   //std::cout << "Part 2:" << result << std::endl;
