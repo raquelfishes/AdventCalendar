fcc83cc93432c579dfb8935f75a5165781217760
Raquel <raquelpm23@gmail.com>
2025-03-27 23:26:01 +0100

    add 2024 design

--- events/2024/design.h
+++ events/2024/design.h
@@ -3,6 +3,35 @@
 template<>
 void printDesign<2024>()
 {
-    std::cout << "Diseño específico para 2024\n";
     // Implementación completa aquí
+
+    printf( HGRN"Advent of Code\n" GRN"\ty(" HGRN"2024" GRN")" reset "\n" );
+    
+      printf( "          .------.          .------------------." reset "\n" );
+      printf( "       .--'      |        .-'                   '-. " reset "  " " 1" BHYEL "  " "**" reset "\n" );
+      printf( "    .--'         |        |                       | " reset "  " " 2" BHYEL "  " "**" reset "\n" );
+      printf( ".---'            |        |                       | " reset "  " " 3" BHYEL "  " "**" reset "\n" );
+      printf( "|                |        |                       | " reset "  " " 4" BHYEL "  " "**" reset "\n" );
+      printf( "|                |        |          ..           | " reset "  " " 5" BHYEL "  " "**" reset "\n" );
+      printf( "|                |        |        .'  '.         | " reset "  " " 6" BHYEL "  " "**" reset "\n" );
+      printf( "|                |        |        |    |         | " reset "  " " 7" BHYEL "  " "**" reset "\n" );
+      printf( "|   .--.         |        |        |    |         | " reset "  " " 8" BHYEL "  " "**" reset "\n" );
+      printf( "'---'  |         |        |        |    |         | " reset "  " " 9" BHYEL "  " "**" reset "\n" );
+      printf( "       |         |        |        |    |         | " reset "  " "10" BHYEL "  " "**" reset "\n" );
+      printf( "       |         |        |        |    |         | " reset "  " "11" BHYEL "  " "**" reset "\n" );
+      printf( "       |         |        |        |    |         | " reset "  " "12" BHYEL "  " "**" reset "\n" );
+      printf( "       |         |        |        |    |         | " reset "  " "13" BHYEL "  " "**" reset "\n" );
+      printf( "       |         |        |        |    |         | " reset "  " "14" BHYEL "  " "**" reset "\n" );
+      printf( "       |         |        |        |    |         | " reset "  " "15" BHYEL "  " "**" reset "\n" );
+      printf( "       |         |        |        |    |         | " reset "  " "16" BHYEL "  " "**" reset "\n" );
+      printf( "       |         |        |        |    |         | " reset "  " "17" BHYEL "  " "**" reset "\n" );
+      printf( "       |         |        |        |    |         | " reset "  " "18" BHYEL "  " "**" reset "\n" );
+      printf( "       |         |        |        |    |         | " reset "  " "19" BHYEL "  " "**" reset "\n" );
+      printf( "       |         |        |        '.  .'         | " reset "  " "20" BHYEL "  " "**" reset "\n" );
+      printf( ".------'         '------. |          ''           | " reset "  " "21" BHYEL "  " "**" reset "\n" );
+      printf( "|                       | |                       | " reset "  " "22" BHYEL "  " "**" reset "\n" );
+      printf( "|                       | |                       | " reset "  " "23" BHYEL "  " "**" reset "\n" );
+      printf( "|                       | |                       | " reset "  " "24" BHYEL "  " "**" reset "\n" );
+      printf( "|                       | '-.                   .-' " reset "  " "25" BHYEL "  " "**" reset "\n" );
+      printf( "'-----------------------'   '-------------------'   " reset "\n" );
 }
